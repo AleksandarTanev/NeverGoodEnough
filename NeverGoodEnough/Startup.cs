812a9c998ab0512cb1f4ac913bab31015d448cc5
Aleksandar Tanev <aleksander.tanev.51@gmail.com>
2017-04-24 01:02:36 +0300

    Initial commit

---
+++ NeverGoodEnough/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeverGoodEnough.Startup))]
+namespace NeverGoodEnough
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
